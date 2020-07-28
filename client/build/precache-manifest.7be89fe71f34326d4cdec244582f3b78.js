ba875b3077526957230f8a48fc72e82ea4922d55
AllAroundD <AllAroundDWeb@gmail.com>
2020-07-28 15:58:53 -0400

    initial setup of server and client

---
+++ client/build/precache-manifest.7be89fe71f34326d4cdec244582f3b78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "715932a6c4d93c067404",
+    "url": "/static/css/main.36403b49.chunk.css"
+  },
+  {
+    "revision": "715932a6c4d93c067404",
+    "url": "/static/js/main.2ee28493.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f5a0adc8d0bc3fbf7243",
+    "url": "/static/js/2.5145a938.chunk.js"
+  },
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "446d19558e855935ce0547aed910a4fb",
+    "url": "/index.html"
+  }
+];
