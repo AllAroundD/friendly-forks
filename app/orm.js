92959a30ee9d97adce146b1bf52a833ba29450bc
Christine Clemens <christineclemens.tesol@gmail.com>
2020-08-01 16:31:00 -0400

    Continued coding model functions, updated dependencies.

--- app/orm.js
+++ app/orm.js
@@ -61,6 +61,18 @@ class DB {
         })
     }
      
+    addEvent(eventData) {
+        const queryString = `INSERT INTO events (eventDate, availableSeats, eventNotes, restrictions)
+        VALUES ('${eventData.eventDate}', '${eventData.availableSeats}', '${eventData.eventNotes}', '${restrictions}')`
+        console.log(`[addEvent] queryString`, queryString);
+        return new Promise ((resolve, reject) => {
+            this.connection.query(queryString, function (err, rows) {
+                if (err) reject (err)
+                resolve(rows)
+            })
+        })
+    }
+
     removeOne(tableName, column, value) {
         return new Promise((resolve, reject) => {
             this.connection.query("DELETE FROM ?? WHERE ?? = ?", [tableName, column, value], function (err, rows) {
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useLocation } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import { useGlobalStore } from "./GlobalStore";
 import { Redirect } from 'react-router-dom';
 import API from "./API";
