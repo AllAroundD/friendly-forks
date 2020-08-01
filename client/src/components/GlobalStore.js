4ccd2b4d8bd15c2b821d06bba0c149e8c4872a14
AllAroundD <AllAroundDWeb@gmail.com>
2020-08-01 16:31:07 -0400

    updated components to get app working initially

--- client/src/components/GlobalStore.js
+++ client/src/components/GlobalStore.js
@@ -1,4 +1,4 @@
-import React, { useContext, useReducer } from "react";
+import React, { useContext, useReducer } from 'react'
 
 // default global store
 const defaultGlobalStore = { messageType: '', message: '', serverStatus: '', loggedIn: false };
@@ -7,51 +7,51 @@ const GlobalData = React.createContext();
 
 let lastDispatcher = { do: '', time: 0 };
 
-function dispatcher(state,action){
-    console.log( `[dispatcher] called with ${action.do}` );
-    if( action.do==lastDispatcher.do && (Date.now()-lastDispatcher.time)<1000 ){
-        console.log( `.. dispatch times too close for ${action.do}; ignoring it.` );
+function dispatcher(state, action) {
+    console.log(`[dispatcher] called with ${action.do}`);
+    if (action.do === lastDispatcher.do && (Date.now() - lastDispatcher.time) < 1000) {
+        console.log(`.. dispatch times too close for ${action.do}; ignoring it.`);
         return state;
     }
     lastDispatcher = { do: action.do, time: Date.now() };
-    
-    let newState = {...state};
-    switch( action.do ){
+
+    let newState = { ...state };
+    switch (action.do) {
         case 'setMessage':
             newState.messageType = action.type;
             newState.message = action.message;
-            console.log( `[GlobalStore:setMessage] set our message: ${action.message}` );
+            console.log(`[GlobalStore:setMessage] set our message: ${action.message}`);
             return newState;
-        
+
         case 'clearMessage':
             newState.messageType = ''; newState.message = '';
             return newState;
 
         case 'loginState':
             newState.loggedIn = action.loggedIn;
-            if( action.loggedIn===false ) // clear the globalData on logout
-                newState = {...defaultGlobalStore};
+            if (action.loggedIn === false) // clear the globalData on logout
+                newState = { ...defaultGlobalStore };
             return newState;
 
         case 'setUserData':
-            newState = {...newState, ...action.data };
+            newState = { ...newState, ...action.data };
             return newState;
 
         default:
-            console.log( `[ERROR] Sorry, unknown do-action: ${action.do}` );
-        break;
+            console.log(`[ERROR] Sorry, unknown do-action: ${action.do}`);
+            break;
     }
 }
 
-function GlobalStore( props ){
-    const [ globalData, dispatch ]= useReducer( dispatcher, defaultGlobalStore );
-    
+function GlobalStore(props) {
+    const [globalData, dispatch] = useReducer(dispatcher, defaultGlobalStore);
+
     return (
-        <GlobalData.Provider value={[ globalData, dispatch ]} {...props} />
+        <GlobalData.Provider value={[globalData, dispatch]} {...props} />
     )
 }
 
-function useGlobalStore(){
+function useGlobalStore() {
     return useContext(GlobalData);
 }
 
