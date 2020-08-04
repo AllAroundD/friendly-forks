dbdd2f8912b677f289820c531fbd03522e710c4b
Shayanne Samarasinghe <shayannesamarasinghe.com>
2020-08-04 15:36:31 -0400

    Added basic html to both response and request message js

---
+++ client/src/components/RequestMessage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const RequestMessage = () => {
+    return (
+        <div>
+            <div class="input-group input-group-lg">
+                <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg"></input>
+            </div>
+            <button type="button" class="btn btn-primary">Respond</button>
+
+
+        </div>
+    )
+}
+
+export default RequestMessage;
--- client/src/components/ResponseMessage.js
+++ client/src/components/ResponseMessage.js
@@ -3,10 +3,15 @@ import React from 'react'
 const ResponseMessage = () => {
     return (
         <div>
-        
+            <div class="input-group input-group-lg">
+                <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg"></input>
+            </div>
+            <button type="button" class="btn btn-primary">Respond</button>
 
-            
-        </div>
+
+         </div>
+
+     
     )
 }
 
