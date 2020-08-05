e051c8542e63daf64aff46b641d4319c49de4c49
Christine Clemens <christineclemens.tesol@gmail.com>
2020-08-05 12:11:38 -0400

    Added AboutCards.js and populated the file.

---
+++ client/src/components/AboutCards.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const AboutCards = (props) => {
+    return (
+        <div>
+            <section className="hero-section" target="_blank">
+                <div className="card-grid">
+                    <a className="card" href="https://www.linkedin.com/in/shayanne-samarasinghe-48569111a/">
+                    <div className="card__background" style={{backgroundImage: 'url(https://avatars3.githubusercontent.com/u/64742100?s=400&u=38ef82a896aa1b3e518a544f1a6e658f0b0f0163&v=4)'}}></div>
+                    <div className="card__content">
+                        <h5 className="card__heading">Shayanne Samarasinghe</h5>
+                        <p className="card__category">Full-stack Developer</p>
+                    </div>
+                    </a>
+                    <a className="card" href="https://www.linkedin.com/in/doug-moore-dev/" target="_blank">
+                    <div className="card__background" style={{backgroundImage: 'url(https://avatars3.githubusercontent.com/u/64918107?s=400&u=4277fa2bf868713adec524f08700cee517941e82&v=4)'}}></div>
+                    <div className="card__content">
+                        <h5 className="card__heading">Doug Moore</h5>
+                        <p className="card__category">Full-stack Developer</p>
+                    </div>
+                    </a>
+                    <a className="card" href="https://www.linkedin.com/in/christinemclemens/" target="_blank">
+                    <div className="card__background" style={{backgroundImage: 'url(https://avatars0.githubusercontent.com/u/64107231?s=460&u=6e6a4ed7a86a7651deae06c450442e76a1bca446&v=4)'}}></div>
+                    <div className="card__content">
+                        <h5 className="card__heading">Christine Clemens</h5>
+                        <p className="card__category">Full-stack Developer</p>
+                    </div>
+                    </a>
+                    <a className="card" href="https://www.linkedin.com/in/saquib-ahmed-985933125/"  target="_blank">
+                    <div className="card__background" style={{backgroundImage: 'url(https://avatars1.githubusercontent.com/u/64999092?s=400&u=4b2277cde7a925bbbe22f1e29b213cbe4d05a954&v=4)'}}></div>
+                    <div className="card__content">
+                        <h5 className="card__heading">Saquib Ahmed</h5>
+                        <p className="card__category">Frontend Developer</p>
+                    </div>
+                    </a>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default AboutCard;
--- client/src/pages/landing.js
+++ client/src/pages/landing.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Jumbotron from '../components/Jumbotron'
 
 export const landing = () => {
     return (
         <div>
+            <Jumbotron></Jumbotron>
             <div class="bgimg-1 w3-display-container w3-opacity-min" id="home">
                 <div class="w3-display-middle" style={{whitepace: 'nowrap'}}>
                     <span class="w3-center w3-padding-large w3-black w3-xlarge w3-wide w3-animate-opacity">Friendly<span class="w3-hide-small" style={{img: 'url(./public/assets/img/FFLogo.png)'}}></span>Forks</span>
