d44c3d8ce6df56335e953fbc188091d85263db9d
AllAroundD <AllAroundDWeb@gmail.com>
2020-08-06 17:16:42 -0400

    commented userModel and eventModel in orm and added url to server

--- app/orm.js
+++ app/orm.js
@@ -1,10 +1,10 @@
 const {connectDB, closeDB} = require("./connection")
 const fs = require('fs')
 const dbFile = './medialist.data'
-const UserModel = require('./models/UserModel')
-const EventModel = require('./models/EventModel')
-const userModel = new UserModel()
-const eventModel = new EventModel()
+// const UserModel = require('./models/UserModel')
+// const EventModel = require('./models/EventModel')
+// const userModel = new UserModel()
+// const eventModel = new EventModel()
 
 class DB {
     // constructor() {
