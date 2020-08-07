03b5f326539a62ad5d4e08a52785d8b6d0f90412
AllAroundD <AllAroundDWeb@gmail.com>
2020-08-07 09:50:37 -0400

    updated login for Prod

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -6,9 +6,7 @@ import { useGlobalStore } from "./GlobalStore";
 
 function LoginPage(){
     // DECLARATIVE FORM OF PROGRAMMING
-    // TODO remove hard-coding once ready
-    // const [ userData, setUserData ] = useState({ name: "", email: localStorage.email, password: "", rememberMe: true });
-    const [ userData, setUserData ] = useState({ name: "JohnDoe", email: 'john.doe@gmail.com', password: "testing123", rememberMe: false });
+    const [ userData, setUserData ] = useState({ name: "", email: localStorage.email, password: "", rememberMe: true });
     const [ globalData, dispatch ] = useGlobalStore();
 
     const inputEmail = useRef();
