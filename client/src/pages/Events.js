c0e39da4870a214594eba36f79e96b7eccb9b5fa
AllAroundD <AllAroundDWeb@gmail.com>
2020-08-07 06:12:11 -0400

    fixed conficts

--- client/src/pages/Events.js
+++ client/src/pages/Events.js
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-import React from 'react'
-// import AddEvent from "../components/AddEvent"
-// import API from '../components/API'
-// import EventBox from '../components/EventBox'
-=======
 import React, {useEffect, useState} from 'react'
 import AddEvent from "../components/AddEvent"
 import API from '../components/API'
 import EventBox from '../components/EventBox'
->>>>>>> christine
 
 function Events (props) {
     let [eventData, setEventData] = useState({ eventDate: "", eventDate: "", availableSeats: "", eventNotes: "", restrictions: "", hostID:"", attendeeID:"" });
